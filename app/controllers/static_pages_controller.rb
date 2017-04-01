53dbdc5c21aee39e936622591c7bfcb5784e0970
Ovi Turcu <ovi_tsb@yahoo.com>
2017-04-01 01:53:43 -0400

    Adding DB and scaffolding

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
+    #@featured_product = Product.first
+    @products = Product.last(5)
+  end
+
+  def landing_page
+    @products = Product.last(5)
   end
 end
